8b2f847871c4d51f1b6a2018d55eb3798d7c4329
Mike Kazmier <kaz@kaztopia.com>
2017-04-19 16:03:07 -0600

    fix bug

--- app/views/components/pageflow/admin/members_tab.rb
+++ app/views/components/pageflow/admin/members_tab.rb
@@ -6,47 +6,47 @@ module Pageflow
                              blank_slate_text: I18n.t('pageflow.admin.entries.no_members')) do
           table_for_collection class: 'memberships', sortable: true, i18n: Pageflow::Membership do |membership|
             column :user, sortable: "#{Pageflow.config.user_class.underscore.pluralize}.last_name", class: 'name'
-              if authorized? :read, membership.user
-                link_to(membership.user.formal_name, send("admin_#{Pageflow.config.user_class.underscore}_path", membership.user),
-                        class: 'view_creator')
-              else
-                membership.user.full_name
-              end
+            if authorized? :read, membership.user
+              link_to(membership.user.formal_name, send("admin_#{Pageflow.config.user_class.underscore}_path", membership.user),
+                      class: 'view_creator')
+            else
+              membership.user.full_name
             end
-            column :role,
-                   sortable: 'pageflow_memberships.role',
-                   title: I18n.t('activerecord.attributes.pageflow/membership.role') do |membership|
-              membership_role_with_tooltip(membership.role, scope: 'entries')
-            end
-            column :created_at, sortable: 'pageflow_memberships.created_at'
-            column do |membership|
-              if authorized?(:update, membership)
-                link_to(I18n.t('pageflow.admin.users.edit_role'),
-                        edit_admin_entry_membership_path(entry,
-                                                         membership,
-                                                         entity_type: :entry),
-                        data: {
+          end
+          column :role,
+                 sortable: 'pageflow_memberships.role',
+                 title: I18n.t('activerecord.attributes.pageflow/membership.role') do |membership|
+            membership_role_with_tooltip(membership.role, scope: 'entries')
+          end
+          column :created_at, sortable: 'pageflow_memberships.created_at'
+          column do |membership|
+            if authorized?(:update, membership)
+              link_to(I18n.t('pageflow.admin.users.edit_role'),
+                      edit_admin_entry_membership_path(entry,
+                                                       membership,
+                                                       entity_type: :entry),
+                      data: {
                           rel: "edit_entry_membership_#{membership.role}"
-                        })
-              end
+                      })
             end
-            column do |membership|
-              if authorized?(:destroy, membership)
-                link_to(I18n.t('pageflow.admin.entries.remove'),
-                        admin_entry_membership_path(membership.entity, membership),
-                        method: :delete,
-                        data: {
+          end
+          column do |membership|
+            if authorized?(:destroy, membership)
+              link_to(I18n.t('pageflow.admin.entries.remove'),
+                      admin_entry_membership_path(membership.entity, membership),
+                      method: :delete,
+                      data: {
                           confirm: I18n.t('active_admin.delete_confirmation'),
                           rel: "delete_membership_#{membership.role}"
-                        })
-              end
+                      })
             end
           end
         end
-        para text_node I18n.t('pageflow.admin.resource_tabs.account_editor_hint')
-        if authorized?(:add_member_to, entry)
-          add_membership_button_if_needed(current_user, entry, 'entry')
-        end
+      end
+
+      para text_node I18n.t('pageflow.admin.resource_tabs.account_editor_hint')
+      if authorized?(:add_member_to, entry)
+        add_membership_button_if_needed(current_user, entry, 'entry')
       end
     end
   end
